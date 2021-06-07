b478748e4ba601d2265cca4701f69b7e8d026344
Pham Thang <bachangngheo9a@gmail.com>
2021-06-07 22:20:11 +0700

    Complete state pattern

--- src/main/java/entity/shipping/ShippingConfigs.java
+++ src/main/java/entity/shipping/ShippingConfigs.java
@@ -4,19 +4,8 @@ package entity.shipping;
  * @author
  */
 public class ShippingConfigs {
-	// commom và content coupling do để dữ liệu ở dạng public đẫn đến có thể lấy và thay đổi bất cứ lúc nào
-    public static String[] PROVINCES = {
-            "Bắc Giang", 	"Bắc Kạn", 			"Cao Bằng", 	"Hà Giang", 	"Lạng Sơn", 	"Phú Thọ",
-            "Quảng Ninh", 	"Thái Nguyên", 		"Tuyên Quang", 	"Yên Bái", 		"Điện Biên", 	"Hòa Bình",
-            "Lai Châu", 	"Sơn La", 			"Bắc Ninh", 	"Hà Nam", 		"Hải Dương", 	"Hưng Yên",
-            "Nam Định", 	"Ninh Bình", 		"Thái Bình", 	"Vĩnh Phúc", 	"Hà Nội", 		"Hải Phòng",
-            "Hà Tĩnh", 		"Nghệ An", 			"Quảng Bình", 	"Quảng Trị", 	"Thanh Hóa", 	"Thừa Thiên-Huế",
-            "Đắk Lắk", 		"Đắk Nông", 		"Gia Lai", 		"Kon Tum", 		"Lâm Đồng", 	"Bình Định",
-            "Bình Thuận", 	"Khánh Hòa", 		"Ninh Thuận", 	"Phú Yên", 		"Quảng Nam", 	"Quảng Ngãi",
-            "Đà Nẵng", 		"Bà Rịa-Vũng Tàu", 	"Bình Dương", 	"Bình Phước", 	"Đồng Nai", 	"Tây Ninh",
-            "Hồ Chí Minh", 	"An Giang", 		"Bạc Liêu", 	"Bến Tre", 		"Cà Mau", 		"Đồng Tháp",
-            "Hậu Giang", 	"Kiên Giang", 		"Long An", 		"Sóc Trăng", 	"Tiền Giang", 	"Trà Vinh",
-            "Vĩnh Long", 	"Cần Thơ" };
+	// commom vÃ  content coupling do Ä‘á»ƒ dá»¯ liá»‡u á»Ÿ dáº¡ng public Ä‘áº«n Ä‘áº¿n cÃ³ thá»ƒ láº¥y vÃ  thay Ä‘á»•i báº¥t cá»© lÃºc nÃ o
+    public static String[] PROVINCES = {"Hà Nội"};
 
     public static int[] RUSH_SUPPORT_PROVINCES_INDEX = {22, 48};
 }
